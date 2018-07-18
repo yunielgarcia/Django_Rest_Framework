3179a04e8c8c5b9a5d161188a8eac6954f7ce5e5
Yuniel Garcia <garciayuni84@gmail.com>
2018-07-18 18:00:47 -0400

    first serializer

--- courses/serializers.py
+++ courses/serializers.py
@@ -4,9 +4,23 @@ from . import models
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
+        extra_kwargs = {
+            'email': {'write_only': True}
+        }
+
+        fields = (
+            'id',
+            'course',
+            'name',
+            'email',
+            'review',
+            'rating',
+            'created_at'
+        )
         model = models.Review
 
 
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Course
+
