aca6eba37acac0e74c7f0e5d64ee750099c063dc
Yuniel Garcia <garciayuni84@gmail.com>
2018-07-20 15:17:33 -0400

    customizing validation

--- courses/serializers.py
+++ courses/serializers.py
@@ -13,14 +13,30 @@ class ReviewSerializer(serializers.ModelSerializer):
             'course',
             'name',
             'email',
-            'review',
+            'comment',
             'rating',
             'created_at'
         )
         model = models.Review
 
+    def validate_rating(self, value):
+        if value in range(1, 6):
+            return value
+        raise serializers.ValidationError(
+            'Rating must be an integer between 1 and 5'
+        )
+
 
 class CourseSerializer(serializers.ModelSerializer):
+    # reviews = ReviewSerializer(many=True, read_only=True)
+    reviews = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='apiv2:review-detail')
+
     class Meta:
         model = models.Course
+        fields = (
+            'id',
+            'title',
+            'url',
+            'reviews',
+        )
 
--- ed_reviews/settings.py
+++ ed_reviews/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'courses',
 ]
 
@@ -125,9 +126,20 @@ STATIC_URL = '/static/'
 # REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+    'DEFAULT_THROTTLE_CLASSES': (
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minutes',
+        'user': '10/minute'
+    }
+
 }
